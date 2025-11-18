c2af8515f33e130d333da0f76b67ecdb69192120
bishnudahal <bdahal17@gmail.com>
2025-11-18 00:59:05 -0500

    image fix

--- backend/src/main/java/com/planner/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/planner/backend/config/JwtAuthenticationFilter.java
@@ -64,16 +64,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 path.equals("/index.html") ||
                 path.startsWith("/static/") ||
                 path.startsWith("/assets/") ||
-                path.startsWith("/h2-console") ||
-                path.startsWith("img/") ||
-                path.startsWith("/img") ||
-                path.startsWith("images/") ||
-                path.startsWith("/images") ||
-                path.startsWith("image/") ||
-                path.startsWith("/image") ||
-                path.startsWith("image") ||
-                path.startsWith("images") ||
-                path.startsWith("img") ||
                 path.matches(".*\\.(js|css|ico|png|jpg|jpeg|svg|woff|woff2|ttf|map)$") ||
                 path.equals("/api/user/login") ||
                 path.equals("/api/user/register") ||
